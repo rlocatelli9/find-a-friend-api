835e5ade0c0c12ed59b414345c6bcca0feb4f072
Robson Locatelli <rlocatelli9@gmail.com>
2024-02-24 12:03:46 -0300

    test: created unit test for the Posts

---
+++ src/cases/posts/register.spec.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import PetsFakeRepository from 'src/repositories/fake/pets-repository'
+import { randomUUID } from 'node:crypto'
+import RegisterPostCase from './register'
+import PostsFakeRepository from 'src/repositories/fake/posts-repository'
+
+let fakeRepository: PostsFakeRepository
+let sut: RegisterPostCase
+
+describe('Post Register Case', () => {
+
+  beforeEach(() => {
+    fakeRepository = new PostsFakeRepository()
+    sut = new RegisterPostCase(fakeRepository)
+  })
+
+  it("should be able to regiter a post", async () => {
+    const { post } = await sut.execute({
+      title: 'Adoção de Pet',
+      description: 'Pet muito fofo',
+      owner_id: 'fake_owner_1',
+      pet_id: 'fake_pet_1',
+      published_at: new Date(),
+    })
+
+    expect(post).toEqual(
+      expect.objectContaining({
+        title: 'Adoção de Pet',
+        owner_id: 'fake_owner_1',
+        pet_id: 'fake_pet_1',
+      })
+    )
+  })
+})
