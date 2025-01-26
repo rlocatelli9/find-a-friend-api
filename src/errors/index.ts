7c5e2c2f5b7f3fdae5e8a6f2b3d14e7c1687997f
Robson Locatelli <rlocatelli9@gmail.com>
2025-01-26 12:48:29 -0300

    refactor: create new class Error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,3 +1,4 @@
 export {UserAlreadyExistsError} from './user-already-exists'
 export {ResourceNotFoundError} from './resource-not-found'
 export {InvalidCredentialsError} from './invalid-credentials'
+export {ResourceAlreadyExistsError} from './resource-already-exists'
